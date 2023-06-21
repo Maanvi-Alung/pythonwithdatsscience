2852ebd91e15dd06156e297ad63335d15873771e
maanvi <maanvialung21@gmail.com>
2023-06-21 14:05:27 +0530

    star

--- t2.py
+++ t2.py
@@ -4,12 +4,15 @@ from turtle import *
 speed('slow')
 distance=100
 side=6
+fillcolor('cyan')   
+begin_fill()
+
 for i in range(side):
     pencolor('red')
     fd(distance)
     rt(120)
     bk(100)
     lt(60)
-    
+end_fill()    
 hideturtle()
 mainloop()
