c9c49c3c1f02a6822b397d4de2c78e6d16d53002
maanvi <maanvialung21@gmail.com>
2023-06-21 13:43:48 +0530

    star

---
+++ t2.py
@@ -0,0 +1,15 @@
+
+from turtle import *
+
+speed('slow')
+distance=100
+side=6
+for i in range(side):
+    pencolor('red')
+    fd(distance)
+    rt(120)
+    bk(100)
+    lt(60)
+    
+hideturtle()
+mainloop()
